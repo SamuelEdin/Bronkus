8ac08e502c29e2672b7b50a8eb04436d118f6891
Smul <samuel.edin@skola.taby.se>
2023-12-04 13:57:29 +0100

    working character customization

--- Assets/Scripts/CharacterCustomization.cs
+++ Assets/Scripts/CharacterCustomization.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterCustomization : MonoBehaviour
 {
+
+    [SerializeField] private int index;
 
     public GameObject startGame;
     public GameObject faces;
     public GameObject colours;
 
-    public GameObject Player;
+    public GameObject PlayerImage;
+    public GameObject face;
+    private Image faceImage;
+    private Image colourImage;
+    public GameObject colour;
 
-    private Sprite selectedFace;
-    private Sprite selectedColour;
+    //public Sprite[] Images;
+    public Texture2D[] Textures;    
 
     void Start()
     {
         faces.SetActive(false);
         colours.SetActive(false);
+        //PlayerImage.SetActive(false);
+
+        colourImage = colour.GetComponent<Image>();
+        faceImage = face.GetComponent<Image>();
+
     }
 
     public void ShowColours()
@@ -25,6 +37,7 @@ public class CharacterCustomization : MonoBehaviour
         colours.SetActive(true);
         faces.SetActive(false);
         startGame.SetActive(false);
+        //PlayerImage.SetActive(true);
     }
 
     public void ShowFaces()
@@ -32,17 +45,22 @@ public class CharacterCustomization : MonoBehaviour
         colours.SetActive(false);
         faces.SetActive(true);
         startGame.SetActive(false);
+        //PlayerImage.SetActive(true);
     }
 
-    public void SelectFace(Sprite faceSprite)
+    public void UpdatePreview(int index)
     {
-        selectedFace = faceSprite;
+        //faceImage.sprite = Images[index - 1];
+        Texture2D texture = Textures[index - 1];
+        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one * 0.5f);
+        if (index > 6)
+        {
+            colourImage.sprite = sprite;
+        } else
+        {
+            faceImage.sprite = sprite;
+        }
     }
 
-    public void SelectColoor(Sprite colourSprite)
-    {
-        selectedColour = colourSprite;
-    }
 
-    
 }
